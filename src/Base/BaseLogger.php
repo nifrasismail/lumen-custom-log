6e798c54f0eedf55e5c00ebad630119547f8289c
nifrasismail <nifrasismail@gmail.com>
2019-04-16 11:52:38 +0530

    frequent logs added

--- src/Base/BaseLogger.php
+++ src/Base/BaseLogger.php
@@ -14,10 +14,10 @@ use Monolog\Logger;
 
 class BaseLogger extends Logger
 {
-    public function __construct($path, $name = "logger")
+    public function __construct($path, $fileName,$frequent = '', $name = "logger")
     {
         parent::__construct($name);
-        $this->pushHandler(new StreamHandler(storage_path($path)));
+        $this->init($path,$fileName,$frequent);
     }
 
     public function debug($message, array $context = array())
@@ -25,5 +25,25 @@ class BaseLogger extends Logger
         if (env('APP_DEBUG')) {
             parent::debug($message, $context);
         }
+    }
+
+    public function init($path,$fileName,$frequent)
+    {
+        switch ($frequent){
+            case Constant::$DAILY: {
+                $path = $path . '/' . $fileName . '_'. date('Y-m-d') . '.log';
+            }
+            case Constant::$MONTHLY: {
+                $path = $path . '/' . $fileName . '_'. date('Y-m') . '.log';
+            }
+            case Constant::$YEARLY: {
+                $path = $path . '/' . $fileName . '_'. date('Y') . '.log';
+            }break;
+            default: {
+                $path = $path . '/' . $fileName . '.log';
+            }
+        }
+
+        $this->pushHandler(new StreamHandler(storage_path($path)));
     }
 }
---
+++ src/Base/Constant.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Nifrasismail\Logger\Base;
+
+
+class Constant
+{
+    public static $DAILY  = 1;
+    public static $MONTHLY = 2;
+    public static $YEARLY = 3;
+}
